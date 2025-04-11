2fb34db4c52330e2e3bdb0d4fa9ae3feca89bc6c
Bikash Shaw <vshaw138@gmail.com>
2025-04-11 19:02:51 +0530

    Added some common packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,66 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { middleware } from "./middleware"
+import {JWT_SECRET} from "@repo/backend-common/config"
+import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express()
 
+app.use(express.json())
+
+//middleware
+
+
 app.get("/", (req,res)=>{
-    res.send("Hello there")
+    res.send("Hello there!!!")
+})
+
+app.post("/signup", (req,res)=>{
+    const data = CreateUserSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return
+    }
+})
+
+app.post("/signin", (req,res)=>{
+    try{
+        const data = SignInSchema.safeParse(req.body)
+        if(!data.success){
+            res.json({
+                message: "Incorrect Inputs"
+            })
+            return
+        }
+    
+        const userId = req.body.userId
+        const token = jwt.sign({userId}, JWT_SECRET)
+        res.json({
+            token: token
+        })
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: error
+        })
+    }
+})
+
+app.post("/room", middleware, (req,res)=>{
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return
+    }
+
+    res.json({
+        roomId: "123"
+    })
 })
 
 app.listen(4000)
